cd2eb4f262e8526a11c1d7c908b45de60bb788dd
Stefano Riccio <riccio.stefano0@gmail.com>
2020-03-30 20:08:15 +0200

    fatta grafica di create

--- resources/views/admin/posts/create.blade.php
+++ resources/views/admin/posts/create.blade.php
@@ -1,9 +1,24 @@
 @extends('layouts.app')
 @section('content')
+  <div class="container">
+    <div class="row">
+      {{-- <h2>{{Auth::user()->name}}</h2> --}}
+      <form action="{{route('admin.posts.store')}}" method="post">
+        @csrf
+        @method('POST')
+        <div class="form-group">
+          <label for="title">Title</label>
+          <input type="text" name="title">
+        </div>
+        <div class="form-group">
+          <label for="body">Body</label>
+          <textarea class="form-control" name="body" id="body" rows="10" cols="30">
 
-  <form action="{{route('admin.posts.store')}}" method="post">
-    @csrf
-    @method('POST')
-    <input type="hidden" name="user_id" value="{{Auth::id}}">
-  </form>
+          </textarea>
+        </div>
+        {{-- <input type="hidden" name="user_id" value="{{Auth::id()}}"> --}}
+        <button class="btn btn-success" type="submit">Salva</button>
+      </form>
+    </div>
+  </div>
 @endsection
