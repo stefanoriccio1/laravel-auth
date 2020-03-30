a91e6156c424ca6e8af5b32e06da8fd589105e0b
Stefano Riccio <riccio.stefano0@gmail.com>
2020-03-30 18:48:39 +0200

    fatta index dei posts

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -4,13 +4,17 @@ namespace App\Http\Controllers;
 
 use App\Post;
 use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+use Illuminate\Support\Facades\Auth;
 
 class PostController extends Controller
 {
     //utenti guest e admin
     public function index()
     {
-      $posts = Post::all();
+      $posts = Post::where('user_id', Auth::id())->get();
+
+
       return view ('guest.posts.index', compact('posts'));
     }
 }
---
+++ resources/views/admin/posts/index.blade.php
@@ -0,0 +1,29 @@
+@extends('layouts.app')
+@section('content')
+  <table class="table">
+    <thead>
+      <tr>
+        <th>ID</th>
+        <th>Title</th>
+        <th>User id</th>
+        <th>Created at</th>
+        <th>Updated at</th>
+        <th colspan="3"></th>
+      </tr>
+    </thead>
+    <tbody>
+      @foreach ($posts as $post)
+        <tr>
+          <td>{{$post->id}}</td>
+          <td>{{$post->title}}</td>
+          <td>{{$post->user_id}}</td>
+          <td>{{$post->created_at}}</td>
+          <td>{{$post->updated_at}}</td>
+          <td>View</td>
+          <td>Edit</td>
+          <td>Delete</td>
+        </tr>
+      @endforeach
+    </tbody>
+  </table>
+@endsection
