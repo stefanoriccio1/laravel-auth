a1f8eb3a72845535c17f670d8bced94f53cf2194
Stefano Riccio <riccio.stefano0@gmail.com>
2020-03-30 16:56:17 +0200

    cambiate le route per accesso admin e per la welcome guest

--- app/Http/Controllers/Admin/HomeController.php
+++ app/Http/Controllers/Admin/HomeController.php
@@ -16,4 +16,9 @@ class HomeController extends Controller
   {
       $this->middleware('auth');
   }
+
+  public function index()
+  {
+    return view('admin.home');
+  }
 }
--- routes/web.php
+++ routes/web.php
@@ -14,9 +14,13 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('guest.welcome');
 });
 
 Auth::routes();
 
-Route::get('/home', 'HomeController@index')->name('home');
+
+
+Route::prefix('admin')->namespace('Admin')->group(function(){
+  Route::get('/home', 'HomeController@index')->name('home');
+});
