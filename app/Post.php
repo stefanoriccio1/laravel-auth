3f28bc41889a89b6fdb12a904daaf560550e7f14
Stefano Riccio <riccio.stefano0@gmail.com>
2020-03-30 17:28:43 +0200

    messi i fillable nel model

--- app/Post.php
+++ app/Post.php
@@ -6,5 +6,11 @@ use Illuminate\Database\Eloquent\Model;
 
 class Post extends Model
 {
-    //
+    protected $fillable =[
+      'userd_id',
+      'title',
+      'body',
+      'slug',
+      'updated_at'
+    ];
 }
